404744e0713b1ea5bda3e597d6e8532555e85aa8
Neelesh025 <nikkuneelesh5@gmail.com>
2026-04-30 15:36:21 +0530

    Remove commented code and clean up formatting

--- Python_test/Q1_Lists/q1.py
+++ Python_test/Q1_Lists/q1.py
@@ -1,5 +1,3 @@
-# Question 1: Lists
-
 data = [10, 20, 30, 40, 50, 20, 30, 60, [70, 80], 90]
 
 data.append([100, 110])
@@ -22,8 +20,6 @@ print("List after operations:", data)
 print("Popped element:", last_element)
 print("Count of 30:", count_30)
 print("Index of 40:", index_40)
-
-# data.sort()  This will give error because of nested list [70, 80]
 
 flat_data = []
 for item in data:
